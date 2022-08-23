9a32f9264fdbe3c48e0414e48c1413c9142aa935
Sumitha Naidu <sumithanaidu1996@gmail.com>
2022-08-24 00:52:19 +0530

    Initial Code Commit
    Signed-off-by: Sumitha Naidu <sumithanaidu1996@gmail.com>

--- src/todos/todos.controller.ts
+++ src/todos/todos.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
 import { TodosService } from './todos.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
